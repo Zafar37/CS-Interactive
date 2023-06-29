cc05432f1b1dbe2de43d779ac789aac0b76dc666
Zafar Urakov <goliburakov535@gmail.com>
2023-06-29 16:45:50 +0900

    Finished project correst data types

---
+++ N2-Correct-Data-Types/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("#2 - Correct data types \n");
+
+DateTime date = DateTime.Now;
+Console.WriteLine($"Todays date: {date}");
+
+DateTime date2 = new DateTime(1970,01,01);
+Console.WriteLine($"Unix date: {date2}");
+
+byte age = 75;
+Console.WriteLine($"{age} years old");
+
+float money = 35.5F;
+Console.WriteLine($"{money}$");
+
+string nickname = "Max Developer";
+Console.WriteLine(nickname);
