3eaab9932fce5f06d73b08be717908612f4668fc
Zafar Urakov <goliburakov535@gmail.com>
2023-06-29 17:49:13 +0900

    Finished project Dilivery Tracker

---
+++ N3-Delivery-Trakcer/Program.cs
@@ -0,0 +1,14 @@
+﻿Guid[] product = new Guid[5]
+{
+    Guid.NewGuid(),
+    Guid.NewGuid(),
+    Guid.NewGuid(),
+    Guid.NewGuid(),
+    Guid.NewGuid()
+};
+
+Console.WriteLine(product[0]);
+Console.WriteLine(product[1]);
+Console.WriteLine(product[2]);
+Console.WriteLine(product[3]);
+Console.WriteLine(product[4]);
