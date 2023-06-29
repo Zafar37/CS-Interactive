f230f58705cdae61d14d10a0a8209b127bafc6b0
Zafar Urakov <goliburakov535@gmail.com>
2023-06-29 18:26:50 +0900

    Finished project Hotel

---
+++ N4-Hotel/Program.cs
@@ -0,0 +1,18 @@
+﻿int index = 0;
+var NowHotelRooms = new bool[][]
+{
+    new bool[]{false,true},
+    new bool[]{true,true,false,false}
+   
+};
+
+Console.WriteLine("Rooms:")
+
+Console.WriteLine($"Xona: {index+1} Bandmi? {NowHotelRooms[0][index++]} ");
+Console.WriteLine($"Xona: {index+1} Bandmi? {NowHotelRooms[0][index++]} ");
+index= 0;
+Console.WriteLine($"Xona: {index+3} Bandmi? {NowHotelRooms[1][index++]} ");
+Console.WriteLine($"Xona: {index+3} Bandmi? {NowHotelRooms[1][index++]} ");
+Console.WriteLine($"Xona: {index+3} Bandmi? {NowHotelRooms[1][index++]} ");
+Console.WriteLine($"Xona: {index+3} Bandmi? {NowHotelRooms[1][index++]} ");
+
