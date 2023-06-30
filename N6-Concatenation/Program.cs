b4e0090eaf111c11d9c5338ec2de109bd91ace1d
Zafar Urakov <goliburakov535@gmail.com>
2023-07-01 02:14:03 +0900

    Finished project N6-Concatenation

---
+++ N6-Concatenation/Program.cs
@@ -0,0 +1,37 @@
+﻿var singIn = @"Account
+              Sing In
+";
+Console.WriteLine(singIn);
+
+Console.WriteLine("Enter your name and age");
+
+Console.Write("Name: ");
+string userName = Console.ReadLine();
+
+Console.Write("Age: ");
+string userAge = Console.ReadLine();
+
+Console.WriteLine("\nPass the robot test");
+Console.WriteLine("2 + 2 * 2 = ?");
+
+Console.Write("Answer: ");
+byte userAnswer = byte.Parse(Console.ReadLine());
+
+if (userAnswer == 6)
+{
+    var successfully = @$"
+   Name: {userName}
+   Age: {userAge}
+   The test was successful
+
+            Entrance allowed!
+    
+";
+    Console.WriteLine(successfully);
+}
+else
+    Console.WriteLine(@"
+            Entry denied, you have not passed the robot check!
+"
+); 
+
