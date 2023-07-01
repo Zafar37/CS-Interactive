a6b2bbeaf0bd80c383e0b81b4d552e61cb42e8e6
Zafar Urakov <goliburakov535@gmail.com>
2023-07-02 04:16:51 +0900

    Finished project Escape Characters

---
+++ N7-Escape-Characters/Program.cs
@@ -0,0 +1,8 @@
+﻿var loveForCS = "{0}\n{1}\n{2}";
+
+var aboutMe = "Hi, i'm {0}\nI'm {1}\nMy teacher {2}";
+var subtraction = string.Format(aboutMe, "Zafar", "18 years old", "Sultonbek");
+Console.WriteLine(subtraction);
+
+var greetingLove = string.Format(loveForCS, "I", "Love", "C#");
+Console.WriteLine("\n" + greetingLove);
