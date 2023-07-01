f9bf430f52df012fad4f8c29ea248e7679c92cd5
Zafar Urakov <goliburakov535@gmail.com>
2023-07-02 04:52:38 +0900

    Finsihed project Interpolation - Format

---
+++ N8-Interpolation-Formatting/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(value: @"       Support ABC-Team
+
+ Enter your email if you want to change your password
+
+");
+Console.Write("Email:");
+
+string yourEmail = Console.ReadLine();
+
+var emailMassage = "\n\"Email massage:\n{0}";
+var greetingEmail = string.Format(emailMassage, $"{yourEmail}");
+Console.WriteLine(greetingEmail);
+
+var passworUser = "\nWe would like to inform you, that your password was changed.\nYour new password is {0}";
+var greetingPassworUser = string.Format(passworUser, "w#34qj9@");
+Console.WriteLine(greetingPassworUser);
+
+Console.WriteLine("\nSincerely\nABC-Team\"");
