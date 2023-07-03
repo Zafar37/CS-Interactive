ffaf74c8e696669a775d726fd80828a80b29372b
Zafar Urakov <goliburakov535@gmail.com>
2023-07-04 03:23:44 +0900

    Finished project Trim-Padding

---
+++ N20-Trim-Padding/Program.cs
@@ -0,0 +1,20 @@
+﻿//task 1
+
+Console.Write("Enter a name:");
+
+var name = Console.ReadLine();
+Console.WriteLine($"Your name: {name}");
+
+var trimmText = name.Trim().Trim('*');
+Console.WriteLine($"TrimmerText: {trimmText}");
+
+Console.WriteLine();
+
+//task 2
+
+Console.Write("Enter a text: ");
+
+var text  = Console.ReadLine();
+var padText = text.PadLeft(text.Length + 10, '_');
+Console.WriteLine($"PadText: {padText}");
+
