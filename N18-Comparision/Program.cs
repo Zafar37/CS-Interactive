8647246e4ac4b06f052e7f4f8f9c4ca396cee93b
Zafar Urakov <goliburakov535@gmail.com>
2023-07-04 02:36:20 +0900

    Finished project Comparision

---
+++ N18-Comparision/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter a word:");
+Console.Write("Word 1:");
+var textCompareA = Console.ReadLine();
+Console.Write("Word 2:");
+var textCompareB = Console.ReadLine();
+
+
+Console.WriteLine($"Compare result:{textCompareA} {textCompareB} : {textCompareA.CompareTo(textCompareB)}");
+
+// "abc" and "bcd" = -1
+// "abc" and "abc" = 0
+// "Abc" and "abc" = 1//po alfovitu (ketmaketligi).
+
+
+//solishtirish:
+
+
+Console.WriteLine($"Equals result of: {textCompareA} {textCompareB} : {textCompareA.Equals(textCompareB)}");
+Console.WriteLine($"EqualseSequence of result: {textCompareA} {textCompareB} : {textCompareA.SequenceEqual(textCompareB)}");
+Console.WriteLine($"= result of: {textCompareA} {textCompareB} : {textCompareA == textCompareB}");
