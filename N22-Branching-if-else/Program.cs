6fb3be0a0c1b020eabc0e7464ee6a27ad42333f9
Zafar Urakov <goliburakov535@gmail.com>
2023-07-04 23:26:39 +0900

    Finished project If-else

---
+++ N22-Branching-if-else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(@" 
+                              Vote");
+Console.Write("Enter your age:");
+var userAge = int.Parse(Console.ReadLine());
+
+if (userAge >= 18)
+    Console.WriteLine("You can vote in elections!");
+else
+    Console.WriteLine("You can not vote in elections!");
