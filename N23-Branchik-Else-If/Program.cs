8bf07aff33b9a9729dfd4f90f410fc0b613e9468
Zafar Urakov <goliburakov535@gmail.com>
2023-07-04 23:32:55 +0900

    Finished project Else-If

---
+++ N23-Branchik-Else-If/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter a number:");
+
+var number = int.Parse(Console.ReadLine());
+
+if(number == 0)
+{
+    Console.WriteLine("This number is 0");
+}
+else if(number < 0)
+{
+    Console.WriteLine("This nimber is negative");
+}
+else
+{
+    Console.WriteLine("This number is positive");
+}
