9936b544f84e6ba9a408c11f0e327659c2d17c36
Zafar Urakov <goliburakov535@gmail.com>
2023-07-05 04:46:31 +0900

    Finished project Switch-Expression

---
+++ N25-Branching-Switch-Expression/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter the number of the day:");
+
+var numberOfTheDay = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+var expression = numberOfTheDay switch
+{
+ 1 => "Dushanba" + " | " +"Monday",   
+ 2 => "Seshanba" + " | " +"Tuesday",   
+ 3 => "Chorshanba" + " | " +"Wednesday",   
+ 4 => "Payshanba" + " | " +"Thursday",   
+ 5 => "Juma" + " | " +"Friday",   
+ 6 => "Shanba" + " | " +"Saturday",   
+ 7 => "Yakshanba" + " | " +"Sunday",   
+} ;
+Console.WriteLine($"Today is: {expression}");
