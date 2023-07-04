0a8fc16d687cc9874414569c98e957dfb10bca0d
Zafar Urakov <goliburakov535@gmail.com>
2023-07-05 04:45:32 +0900

    Finished project Switch

---
+++ N24-Branching-Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter the number of the day:");
+
+var numberOfTheDay = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+switch (numberOfTheDay)
+{
+    case 1:
+        Console.WriteLine("Today is Monday.");
+        break; 
+    case 2:
+        Console.WriteLine("Today is Tuesday.");
+        break; 
+    case 3:
+        Console.WriteLine("Today is Wednesday.");
+        break; 
+    case 4:
+        Console.WriteLine("Today is Thursday.");
+        break; 
+    case 5:
+        Console.WriteLine("Today is Friday.");
+        break; 
+    case 6:
+        Console.WriteLine("Today is Saturday.");
+        break; 
+    case 7:
+        Console.WriteLine("Today is Sunday.");
+        break;
+}
