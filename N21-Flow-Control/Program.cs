0e7bbbbb742ed85d0f22268723cfd31871506681
Zafar Urakov <goliburakov535@gmail.com>
2023-07-04 23:19:47 +0900

    Finished project Flow-Control

---
+++ N21-Flow-Control/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number:");
+
+var number = int.Parse(Console.ReadLine());
+if (number > 10)
+{
+    Console.WriteLine("This number is greater than 10.");
+}
+else
+    Console.WriteLine("This number is less than 10.");
